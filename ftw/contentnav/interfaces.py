25882940c89b2193c094ed287bb78dc40d9ac36e
Elio Schmutz <e.schmutz@4teamwork.ch>
2015-06-25 10:11:51 +0200

    Implement uninstall and install tests

--- ftw/contentnav/interfaces.py
+++ ftw/contentnav/interfaces.py
@@ -3,7 +3,3 @@ from zope.interface import Interface
 
 class ICategorizable(Interface):
     """Marker interface for categorizable content"""
-
-
-class IFtwContentNavLayer(Interface):
-    """Request marker for ftw.contentnav"""
--- ftw/contentnav/tests/test_setup.py
+++ ftw/contentnav/tests/test_setup.py
@@ -1,6 +1,4 @@
-from ftw.contentnav.interfaces import IFtwContentNavLayer
 from ftw.contentnav.testing import FTW_CONTENTNAV_INTEGRATION_TESTING
-from plone.browserlayer import utils
 from Products.CMFCore.utils import getToolByName
 from unittest2 import TestCase
 
@@ -19,35 +17,6 @@ class TestPackageInstall(TestCase):
         self.assertIn(
             pid, installed, 'package appears not to have been installed')
 
-    def test_browserlayer_registered(self):
-        self.assertIn(IFtwContentNavLayer, utils.registered_layers())
-
     def test_index_get_content_categories_installed(self):
         catalog = getToolByName(self.portal, "portal_catalog")
         self.assertIn('get_content_categories', catalog.indexes())
-
-
-class TestPackageUninstall(TestCase):
-
-    layer = FTW_CONTENTNAV_INTEGRATION_TESTING
-
-    def setUp(self):
-        self.portal = self.layer['portal']
-        self.qi_tool = getToolByName(self.portal, 'portal_quickinstaller')
-        try:
-            self.qi_tool.uninstallProducts(['ftw.contentnav'])
-        except:
-            pass
-
-    def test_package_is_not_installed(self):
-        pid = 'ftw.contentnav'
-        installed = [p['id'] for p in self.qi_tool.listInstalledProducts()]
-        self.assertNotIn(
-            pid, installed, 'package appears not to have been installed')
-
-    def test_browserlayer_no_longer_registered(self):
-        self.assertNotIn(IFtwContentNavLayer, utils.registered_layers())
-
-    # def test_index_get_content_categories_is_removed(self):
-    #     catalog = getToolByName(self.portal, "portal_catalog")
-    #     self.assertNotIn('get_content_categories', catalog.indexes())
